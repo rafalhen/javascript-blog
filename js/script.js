505515d4f51d64e2f427b3a178212ec1bc501c6d
Rafal Hen <rafalhen@poczta.onet.pl>
2020-11-14 17:55:26 +0100

    pentla2

--- js/script.js
+++ js/script.js
@@ -6,8 +6,25 @@ document.getElementById('test-button').addEventListener('click', function(){
     console.log('links:', links);
   });
 
-  const titleClickHandler = function(){
+  const titleClickHandler = function(event){
     console.log('Link was clicked!');
+    console.log('event');
+  
+    /* remove class 'active' from all article links  */
+    const activeLinks = document.querySelectorAll('titles a.active');
+
+    for(let activeLink of activeLinks){
+        activeLink.classList.remove('active');
+    }
+    /* add class 'active' to the clicked link */
+  
+    /* remove class 'active' from all articles */
+  
+    /* get 'href' attribute from the clicked link */
+  
+    /* find the correct article using the selector (value of 'href' attribute) */
+  
+    /* add class 'active' to the correct article */
   }
   
   const links = document.querySelectorAll('.titles a');
@@ -15,4 +32,3 @@ document.getElementById('test-button').addEventListener('click', function(){
   for(let link of links){
     link.addEventListener('click', titleClickHandler);
   }
-  
